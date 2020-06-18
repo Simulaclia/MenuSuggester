2bc20305dbc81806bdbca516621e90139e54ece7
Selelium <65858392+Selelium@users.noreply.github.com>
2020-06-18 13:31:01 +0900

    基本記述更新

--- src/top/seasonsearch/SeasonSearchPage.java
+++ src/top/seasonsearch/SeasonSearchPage.java
@@ -7,26 +7,39 @@ import java.util.ArrayList;
 
 public class SeasonSearchPage {
 	public static void main(String[] args) {
+		String seasonFood = "ねぎ";
 
 		try {
+			// CSVファイル読み込み処理
 			FileInputStream fis = new FileInputStream("csv/Recipe.csv");
 			InputStreamReader isr = new InputStreamReader(fis);
 			BufferedReader br = new BufferedReader(isr);
 
-			String tempData;
-			ArrayList<ArrayList<String>> recipeList = new ArrayList<ArrayList<String>>();
+			String tempData; // CSVファイル一行読み込みデータ
+			ArrayList<ArrayList<String>> recipeList = new ArrayList<ArrayList<String>>(); // 絞り込み後データ
 
-			int i = 0;
-			while ((tempData = br.readLine()) != null) {
-				String[] tempList = tempData.split(",");
+			// CSVファイルリスト代入
+			while ((tempData = br.readLine()) != null) { // CSVファイル内データの最後までループ
+				String[] tempList = tempData.split(","); // CSVファイル一行分から","で分割して配列代入
 				ArrayList<String> tempArrayList = new ArrayList<String>();
-				for (int j = 0; j <= tempList.length - 1; j++) {
-					tempArrayList.add(tempList[j]);
+				// 配列内データをリストに代入
+				for (int i = 0; i <= tempList.length - 1; i++) {
+					tempArrayList.add(tempList[i]);
 				}
-				if (tempArrayList.get(12).equals("ねぎ")) {
-					recipeList.add(tempArrayList);
+				// 旬食材絞り込み
+				if (tempArrayList.get(12).equals(seasonFood)) {
+					recipeList.add(tempArrayList); // 条件に合致したデータを代入
 				}
 			}
+
+			// 絞り込み後データ表示
+			for (ArrayList<String> recipe : recipeList) {
+				for (String data : recipe) {
+					System.out.print(data + "\t");
+				}
+				System.out.println();
+			}
+
 			br.close();
 		} catch (Exception e) {
 			e.printStackTrace();
