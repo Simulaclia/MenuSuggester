13e65e8694d48fb9f56dc2e49ad8d49ea5558447
Selelium <65858392+Selelium@users.noreply.github.com>
2020-06-18 12:22:53 +0900

    SeasonSearchPage基本処理実装

--- src/top/seasonsearch/SeasonSearchPage.java
+++ src/top/seasonsearch/SeasonSearchPage.java
@@ -1,5 +1,35 @@
 package top.seasonsearch;
 
-public class SeasonSearchPage {
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
+public class SeasonSearchPage {
+	public static void main(String[] args) {
+
+		try {
+			FileInputStream fis = new FileInputStream("csv/Recipe.csv");
+			InputStreamReader isr = new InputStreamReader(fis);
+			BufferedReader br = new BufferedReader(isr);
+
+			String tempData;
+			ArrayList<ArrayList<String>> recipeList = new ArrayList<ArrayList<String>>();
+
+			int i = 0;
+			while ((tempData = br.readLine()) != null) {
+				String[] tempList = tempData.split(",");
+				ArrayList<String> tempArrayList = new ArrayList<String>();
+				for (int j = 0; j <= tempList.length - 1; j++) {
+					tempArrayList.add(tempList[j]);
+				}
+				if (tempArrayList.get(12).equals("ねぎ")) {
+					recipeList.add(tempArrayList);
+				}
+			}
+			br.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
