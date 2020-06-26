03effd717b7990ef546a2d67b9066c6e775c4518
Selelium <65858392+Selelium@users.noreply.github.com>
2020-06-26 10:55:52 +0900

    コメントアウト追加

--- src/top/Main.java
+++ src/top/Main.java
@@ -10,8 +10,9 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("TopPage.fxml"));
-			Scene scene = new Scene(root, 1600, 900);
+			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("TopPage.fxml")); // TopPage.fxmi表示
+			Scene scene = new Scene(root, 1600, 900); // 画面サイズ指定
+
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
