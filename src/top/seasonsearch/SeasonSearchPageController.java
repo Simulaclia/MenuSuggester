045db123fd1f7183ad91c807017a03e9d94da818
Selelium <65858392+Selelium@users.noreply.github.com>
2020-07-02 14:43:22 +0900

    料理名画面反映

--- src/top/seasonsearch/SeasonSearchPageController.java
+++ src/top/seasonsearch/SeasonSearchPageController.java
@@ -1,4 +1,20 @@
 package top.seasonsearch;
 
+import java.util.ArrayList;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class SeasonSearchPageController {
+
+	ArrayList<ArrayList<String>> seasonMenu = SeasonSearchPage.SeasonSearch();
+
+	@FXML
+	private Label label1;
+
+	@FXML
+	void initialize() {
+		label1.setText(seasonMenu.get(0).get(0));
+	}
+
 }
