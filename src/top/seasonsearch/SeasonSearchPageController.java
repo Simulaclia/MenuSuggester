fef8d7010b3a62f21fdfa37376072f324dfb2a71
Selelium <65858392+Selelium@users.noreply.github.com>
2020-07-02 14:48:36 +0900

    Topに戻る処理実装

--- src/top/seasonsearch/SeasonSearchPageController.java
+++ src/top/seasonsearch/SeasonSearchPageController.java
@@ -1,9 +1,17 @@
 package top.seasonsearch;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
 public class SeasonSearchPageController {
 
@@ -15,6 +23,24 @@ public class SeasonSearchPageController {
 	@FXML
 	void initialize() {
 		label1.setText(seasonMenu.get(0).get(0));
+	}
+
+	@FXML
+	void ReturnTopButton_OnClick(ActionEvent evt) {
+		try {
+			Scene s = ((Node) evt.getSource()).getScene();
+			Window window = s.getWindow();
+			window.hide();
+
+			AnchorPane topPage = (AnchorPane) FXMLLoader.load(getClass().getResource("/top/TopPage.fxml"));
+			Scene scene = new Scene(topPage);
+			Stage stage = new Stage();
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
 	}
 
 }
