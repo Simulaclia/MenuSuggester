94ea13941a51555274e69f4a34912f4cdeb4cf42
Selelium <65858392+Selelium@users.noreply.github.com>
2020-07-02 11:14:13 +0900

    修正

--- src/top/seasonsearch/SeasonSearchPageController.java
+++ src/top/seasonsearch/SeasonSearchPageController.java
@@ -1,19 +1,4 @@
-//package top.seasonsearch;
-//
-//import java.io.IOException;
-//
-//import javafx.fxml.FXMLLoader;
-//import javafx.scene.Scene;
-//import javafx.scene.layout.AnchorPane;
-//
-//public class SeasonSearchPageController {
-//	public void SeasonSearch_onClisk() {
-//		try {
-//			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("SeasonSearchPage.fxml"));
-//			Scene scene = new Scene(root, 1600, 900); // 画面サイズ指定
-//		} catch (IOException e) {
-//			// TODO 自動生成された catch ブロック
-//			e.printStackTrace();
-//		}
-//	}
-//}
+package top.seasonsearch;
+
+public class SeasonSearchPageController {
+}
