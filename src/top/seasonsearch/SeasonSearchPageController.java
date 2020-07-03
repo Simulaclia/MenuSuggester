125ea4cc2c7d2e216d18522f8c68ef1ad2abbbc0
sintomo <50051678+sintomo@users.noreply.github.com>
2020-07-03 14:35:24 +0900

    ひろとくん

--- src/top/seasonsearch/SeasonSearchPageController.java
+++ src/top/seasonsearch/SeasonSearchPageController.java
@@ -1,19 +1,74 @@
-//package top.seasonsearch;
-//
-//import java.io.IOException;
-//
-//import javafx.fxml.FXMLLoader;
-//import javafx.scene.Scene;
-//import javafx.scene.layout.AnchorPane;
-//
-//public class SeasonSearchPageController {
-//	public void SeasonSearch_onClisk() {
-//		try {
-//			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("SeasonSearchPage.fxml"));
-//			Scene scene = new Scene(root, 1600, 900); // 画面サイズ指定
-//		} catch (IOException e) {
-//			// TODO 自動生成された catch ブロック
-//			e.printStackTrace();
-//		}
-//	}
-//}
+package top.seasonsearch;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+public class SeasonSearchPageController {
+
+	@FXML
+	private Label menuName1, menuName2, menuName3, country1, country2, country3, genre1, genre2, genre3, time1, time2,
+			time3, cal1, cal2, cal3, seasonFood1, seasonFood2, seasonFood3;
+
+	Random rand = new Random();
+
+	@FXML
+	void initialize() {
+		Label[][] label = { { menuName1, menuName2, menuName3 }, { country1, country2, country3 },
+				{ genre1, genre2, genre3 }, { time1, time2, time3 }, { cal1, cal2, cal3 },
+				{ seasonFood1, seasonFood2, seasonFood3 } };
+		ArrayList<ArrayList<String>> seasonMenu = SeasonSearchPage.SeasonSearch();
+		RandomSetMenu(seasonMenu, label);
+	}
+
+	@FXML
+	void ReSuggest_OnClick(ActionEvent evt) {
+		Label[][] label = { { menuName1, menuName2, menuName3 }, { country1, country2, country3 },
+				{ genre1, genre2, genre3 }, { time1, time2, time3 }, { cal1, cal2, cal3 },
+				{ seasonFood1, seasonFood2, seasonFood3 } };
+		ArrayList<ArrayList<String>> seasonMenu = SeasonSearchPage.SeasonSearch();
+		RandomSetMenu(seasonMenu, label);
+	}
+
+	@FXML
+	void ReturnTopButton_OnClick(ActionEvent evt) {
+		try {
+			Scene s = ((Node) evt.getSource()).getScene();
+			Window window = s.getWindow();
+			window.hide();
+
+			AnchorPane topPage = (AnchorPane) FXMLLoader.load(getClass().getResource("/top/TopPage.fxml"));
+			Scene scene = new Scene(topPage);
+			Stage stage = new Stage();
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+	}
+
+	void RandomSetMenu(ArrayList<ArrayList<String>> seasonMenu, Label[][] label) {
+		for (int i = 0; i <= 2; i++) {
+			int randomValue = rand.nextInt(seasonMenu.size());
+			label[0][i].setText(seasonMenu.get(randomValue).get(0));
+			label[1][i].setText(seasonMenu.get(randomValue).get(1));
+			label[2][i].setText(seasonMenu.get(randomValue).get(2));
+			label[3][i].setText(seasonMenu.get(randomValue).get(4));
+			label[4][i].setText(seasonMenu.get(randomValue).get(5));
+			label[5][i].setText(seasonMenu.get(randomValue).get(12));
+			seasonMenu.remove(randomValue);
+		}
+	}
+
+}
