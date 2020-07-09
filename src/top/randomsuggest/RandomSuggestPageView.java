1ef95778e0cc49852ad1461ed0d7b3dc7fbbd519
The-Reidon <66047096+The-Reidon@users.noreply.github.com>
2020-07-09 11:53:24 +0900

    ランダム献立設計:画面作成

--- src/top/randomsuggest/RandomSuggestPageView.java
+++ src/top/randomsuggest/RandomSuggestPageView.java
@@ -1,5 +1,34 @@
 package top.randomsuggest;
 
-public class RandomSuggestPageView {
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
+public abstract class RandomSuggestPageView extends Application {
+
+	//BaseView<GridPane>
+
+	//Topページ⇒ランダム献立提案のボタン
+	//fx:id[RandumSuggest]
+
+	//@Override
+	public void Init() {
+		try {
+			AnchorPane tbutton = (AnchorPane) FXMLLoader.load(getClass().getResource("RandomSuggestPage.fxml"));
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		//Button button = (Button)getObject("button");
+		//Button button = (Button) getObject("topbutton");
+		//button.setText("ボタン");
+	}
 
 }
+/*CheckBox checkBox = (CheckBox) getObject("checkbox");
+checkBox.setText("チェックボックス");
+
+Label label = (Label) getObject("label");
+label.setText("ラベル");*/
