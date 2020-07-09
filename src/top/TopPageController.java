b0bc4589db166d3ebd2814391fcee20cc0f76cd5
Selelium <65858392+Selelium@users.noreply.github.com>
2020-07-09 15:50:18 +0900

    修正

--- src/top/TopPageController.java
+++ src/top/TopPageController.java
@@ -1,31 +1,30 @@
 package top;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class TopPageController {
 	@FXML
-	void RandomSuggest_onClick(ActionEvent evt) {
-		new TopPage().changePage("/top/randomsuggest/RondomSuggestPage.fxml");
+	void RandomSuggest_onClick() {
+		new TopPage().changePage("/top/randomsuggest/RandomSuggestPage.fxml");
 	}
 
 	@FXML
-	void WeekSuggest_onClick(ActionEvent evt) {
+	void WeekSuggest_onClick() {
 		new TopPage().changePage("/top/weeksuggest/WSPage.fxml");
 	}
 
 	@FXML
-	void FoodSerach_onClick(ActionEvent evt) {
+	void FoodSerach_onClick() {
 		new TopPage().changePage("/top/foodsearch/FoodSearchPage.fxml");
 	}
 
 	@FXML
-	void SeasonSearch_onClick(ActionEvent evt) {
+	void SeasonSearch_onClick() {
 		new TopPage().changePage("/top/seasonsearch/SeasonSearchPage.fxml");
 	}
 
 	@FXML
-	void Bookmark_onClick(ActionEvent evt) {
+	void Bookmark_onClick() {
 		new TopPage().changePage("/top/bookmark/BookmarkPage.fxml");
 	}
 }
