62ac896514d49195f838f902a17a98094ae3d9ea
Selelium <65858392+Selelium@users.noreply.github.com>
2020-07-09 13:29:55 +0900

    画面遷移挙動修正

--- src/top/TopPage.java
+++ src/top/TopPage.java
@@ -7,20 +7,27 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class TopPage extends Application {
+	public static Stage stage;
+
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			AnchorPane topPage = (AnchorPane) FXMLLoader.load(getClass().getResource("TopPage.fxml")); // TopPage.fxml表示
-			Scene scene = new Scene(topPage);
-			scene.getStylesheets().add(getClass().getResource("TopPage.css").toExternalForm()); // CSS指定
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage stage) {
+		TopPage.stage = stage;
+		changePage("TopPage.fxml");
+		stage.show();
 	}
 
 	public static void main(String[] args) {
 		launch(args);
+	}
+
+	public void changePage(String fxml) {
+		try {
+			AnchorPane topPage = (AnchorPane) FXMLLoader.load(getClass().getResource(fxml)); // TopPage.fxml表示
+			Scene scene = new Scene(topPage);
+//			scene.getStylesheets().add(getClass().getResource(css).toExternalForm()); // CSS指定
+			stage.setScene(scene);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
