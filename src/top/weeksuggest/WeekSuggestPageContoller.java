bea95e5bbdd3fa445f2686c03b783b634ca66641
Selelium <65858392+Selelium@users.noreply.github.com>
2020-07-10 11:59:54 +0900

    修正

--- src/top/weeksuggest/WeekSuggestPageContoller.java
+++ src/top/weeksuggest/WeekSuggestPageContoller.java
@@ -1,68 +1,34 @@
 package top.weeksuggest;
 
-import java.io.IOException;
-import java.util.Random;
-
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-import javafx.stage.Window;
+import top.TopPage;
 
 public class WeekSuggestPageContoller {
 
-	//ラベルの取得
-	private Label CookMenu1, CookMenu2, CookMenu3, CookMenu4, CookMenu5, CookMenu6, CookMenu7,
-			Genre1, Genre2, Genre3, Genre4, Genre5, Genre6, Genre7,
-			cooking1, cooking2, cooking3, cooking4, cooking5, cooking6, cooking7,
-			time1, time2, time3, time4, time5, time6, time7,
-			calorie1, calorie2, calorie3, calorie4, calorie5, calorie6, calorie7;
+	// // ラベルの取得
+	// private Label CookMenu1, CookMenu2, CookMenu3, CookMenu4, CookMenu5,
+	// CookMenu6, CookMenu7, Genre1, Genre2, Genre3,
+	// Genre4, Genre5, Genre6, Genre7, cooking1, cooking2, cooking3, cooking4,
+	// cooking5, cooking6, cooking7, time1,
+	// time2, time3, time4, time5, time6, time7, calorie1, calorie2, calorie3,
+	// calorie4, calorie5, calorie6,
+	// calorie7;
 
-	Random rand = new Random();
+	// Random rand = new Random();
 
+	// @FXML
+	// void initialize() {
+	// Label[][] label = { { CookMenu1, CookMenu2, CookMenu3, CookMenu4, CookMenu5,
+	// CookMenu6, CookMenu7 },
+	// { Genre1, Genre2, Genre3, Genre4, Genre5, Genre6, Genre7 },
+	// { cooking1, cooking2, cooking3, cooking4, cooking5, cooking6, cooking7 },
+	// { time1, time2, time3, time4, time5, time6, time7 },
+	// { calorie1, calorie2, calorie3, calorie4, calorie5, calorie6, calorie7 } };
+	// }
+
+	// 絞り込み機能に飛ぶ処理
 	@FXML
-	void initialize() {
-		Label[][] label = { { CookMenu1, CookMenu2, CookMenu3, CookMenu4, CookMenu5, CookMenu6, CookMenu7 },
-				{ Genre1, Genre2, Genre3, Genre4, Genre5, Genre6, Genre7 },
-				{ cooking1, cooking2, cooking3, cooking4, cooking5, cooking6, cooking7 },
-				{ time1, time2, time3, time4, time5, time6, time7 },
-				{ calorie1, calorie2, calorie3, calorie4, calorie5, calorie6, calorie7 } };
-		//ArrayList<ArrayList<String>> WeekSuggest = WeekSuggestPage.WeekSuggest();
-		//RandomSetMenu(WeekSuggest, label);
-	}
-
-	//   絞り込み機能に飛ぶ処理
-	@FXML
-	void NextButton_OnClick() {
-		new WeekSuggestPage().chengePage("/top/weeksuggest/WSPNarrow_down.fxml");
-	}
-
-	/*   ブックマーク登録ボタン処理
-	@FXML
-	void bookmark_button_OnClick() {
-	
-	}
-	*/
-
-	@FXML //TOPに戻るボタンの処理
-	void ReturnTopButton_OnClick(ActionEvent evt) {
-		try {
-			Scene s = ((Node) evt.getSource()).getScene();
-			Window window = s.getWindow();
-			window.hide();
-
-			AnchorPane topPage = (AnchorPane) FXMLLoader.load(getClass().getResource("/top/TopPage.fxml"));
-			Scene scene = new Scene(topPage);
-			Stage stage = new Stage();
-			stage.setScene(scene);
-			stage.show();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
+	void ReturnTopButton_OnClick() {
+		new TopPage().changePage("/top/TopPage.fxml");
 	}
 }
