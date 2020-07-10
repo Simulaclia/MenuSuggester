ca85d397cdd117ee7461999c5bae45579bfa89d5
Selelium <65858392+Selelium@users.noreply.github.com>
2020-07-10 11:46:58 +0900

    修正

--- src/top/randomsuggest/RandomSuggestPageController.java
+++ src/top/randomsuggest/RandomSuggestPageController.java
@@ -1,5 +1,12 @@
 package top.randomsuggest;
 
+import javafx.fxml.FXML;
+import top.TopPage;
+
 public class RandomSuggestPageController {
+	@FXML
+	void ReturnTopPage_OnClick() {
+		new TopPage().changePage("/top/TopPage.fxml");
+	}
 
 }
