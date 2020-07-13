7f88a5877f169076c9170a0d5758cb1338ea1627
Selelium <65858392+Selelium@users.noreply.github.com>
2020-07-13 12:23:56 +0900

    ブックマーク1ページ表示処理実装

--- src/top/bookmark/BookmarkPage.java
+++ src/top/bookmark/BookmarkPage.java
@@ -1,11 +1,38 @@
 package top.bookmark;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class BookmarkPage {
+
+	public static ArrayList<ArrayList<String>> Bookmark() {
+		try {
+			FileInputStream fisRec = new FileInputStream("csv/Bookmark.csv");
+			InputStreamReader isrRec = new InputStreamReader(fisRec);
+			BufferedReader brRec = new BufferedReader(isrRec);
+			String tempData;
+			ArrayList<ArrayList<String>> bookmarkList = new ArrayList<ArrayList<String>>();
+
+			while ((tempData = brRec.readLine()) != null) {
+				String[] tempList = tempData.split(",");
+				ArrayList<String> tempArrayList = new ArrayList<String>();
+				for (String tempLi : tempList) {
+					tempArrayList.add(tempLi);
+				}
+				bookmarkList.add(tempArrayList);
+			}
+			brRec.close();
+			return bookmarkList;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 	public static void addBookmark(ArrayList<String> recipe) {
 		try {
@@ -24,5 +51,4 @@ public class BookmarkPage {
 			e.printStackTrace();
 		}
 	}
-
 }
--- src/top/bookmark/BookmarkPageController.java
+++ src/top/bookmark/BookmarkPageController.java
@@ -1,11 +1,41 @@
 package top.bookmark;
 
+import java.util.ArrayList;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import top.TopPage;
 
 public class BookmarkPageController {
 	@FXML
+	private Label menuName1, menuName2, menuName3, genre1, genre2, genre3, cookGenre1, cookGenre2, cookGenre3, time1,
+			time2, time3, cal1, cal2, cal3;
+
+	// ArrayList<ArrayList<String>> bookmarkRecipe;
+
+	// 初期処理
+	@FXML
+	void initialize() {
+		// bookmarkRecipe = new ArrayList<ArrayList<String>>();
+		Label[][] label = { { menuName1, menuName2, menuName3 }, { cookGenre1, cookGenre2, cookGenre3 },
+				{ genre1, genre2, genre3 }, { time1, time2, time3 }, { cal1, cal2, cal3 } };
+		ArrayList<ArrayList<String>> bookmarkMenu = BookmarkPage.Bookmark();
+		bookmarkSetMenu(bookmarkMenu, label);
+	}
+
+	@FXML
 	void ReturnTopPage_OnClick() {
 		new TopPage().changePage("/top/TopPage.fxml");
+	}
+
+	void bookmarkSetMenu(ArrayList<ArrayList<String>> bookmarkMenu, Label[][] label) {
+		for (int i = 0; i <= 2; i++) {
+			label[0][i].setText(bookmarkMenu.get(i).get(0)); // 料理名
+			label[1][i].setText(bookmarkMenu.get(i).get(1)); // 料理国
+			label[2][i].setText(bookmarkMenu.get(i).get(2)); // 料理ジャンル
+			label[3][i].setText(bookmarkMenu.get(i).get(4) + "分"); // 所要時間
+			label[4][i].setText(bookmarkMenu.get(i).get(5) + "kcal"); // カロリー
+			// bookmarkRecipe.add(bookmarkMenu.get(0));
+		}
 	}
 }
