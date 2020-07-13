80a9e084f61bcc3875437063cf75cb1786b3d646
sintomo <50051678+sintomo@users.noreply.github.com>
2020-07-13 11:28:06 +0900

    修正

--- src/top/foodsearch/FoodSearchPageController.java
+++ src/top/foodsearch/FoodSearchPageController.java
@@ -5,8 +5,13 @@ import javafx.fxml.FXML;
 import top.TopPage;
 
 public class FoodSearchPageController {
-	@FXML
+	@FXML //Topボタン
 	void ReturnTopButton_OnClick() {
 		new TopPage().changePage("/top/TopPage.fxml");
+	}
+
+	@FXML //決定ボタン→ansへ
+	void nextPage_OnClick() {
+		new TopPage().changePage("/top/foodsearchFoodSearchPage_ans.fxml");
 	}
 }
