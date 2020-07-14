85cb070ebf35f9577b33fe75b7680320ccfb9bb5
The-Reidon <66047096+The-Reidon@users.noreply.github.com>
2020-07-14 16:09:38 +0900

    RandomSuggestPage:GridPane削除 & ランダム献立絞り込み画面(仮)追加

--- src/top/randomsuggest/RandomSuggestPageController.java
+++ src/top/randomsuggest/RandomSuggestPageController.java
@@ -4,9 +4,20 @@ import javafx.fxml.FXML;
 import top.TopPage;
 
 public class RandomSuggestPageController {
+
+	@FXML
+	void RandomSuggestNarrow_onClick() {
+		new TopPage().changePage("/top/randomsuggest/RandomSuggestNarrowdown.fxml");
+	}
+
 	@FXML
 	void ReturnTopPage_OnClick() {
+		//ここのTopPageは変えなくてよい
 		new TopPage().changePage("/top/TopPage.fxml");
 	}
 
+	@FXML
+	void ReturnRandomSuggestPage_OnClick() {
+		new TopPage().changePage("/top.randomsuggest/RandomSuggestPage.fxml");
+	}
 }
