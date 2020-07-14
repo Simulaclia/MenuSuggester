35bc675a616467fc64c0604a0500ded1f4a5a01d
Selelium <65858392+Selelium@users.noreply.github.com>
2020-07-14 13:57:27 +0900

    コメントアウト追加

--- src/top/bookmark/BookmarkPage.java
+++ src/top/bookmark/BookmarkPage.java
@@ -12,12 +12,15 @@ public class BookmarkPage {
 
 	public static ArrayList<ArrayList<String>> Bookmark() {
 		try {
+			// Bookmark.csvファイル読み込み
 			FileInputStream fisRec = new FileInputStream("csv/Bookmark.csv");
 			InputStreamReader isrRec = new InputStreamReader(fisRec);
 			BufferedReader brRec = new BufferedReader(isrRec);
-			String tempData;
-			ArrayList<ArrayList<String>> bookmarkList = new ArrayList<ArrayList<String>>();
 
+			String tempData;
+			ArrayList<ArrayList<String>> bookmarkList = new ArrayList<ArrayList<String>>(); // Bookmark.csv内のレシピリスト
+
+			// リスト代入処理
 			while ((tempData = brRec.readLine()) != null) {
 				String[] tempList = tempData.split(",");
 				ArrayList<String> tempArrayList = new ArrayList<String>();
@@ -27,62 +30,77 @@ public class BookmarkPage {
 				bookmarkList.add(tempArrayList);
 			}
 			brRec.close();
-			return bookmarkList;
+			return bookmarkList; // Bookmark.csv内レシピリスト返却
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
 
+	// ブックマーク追加処理
 	public static void AddBookmark(ArrayList<String> recipe) {
-		DeleteBookmark(recipe);
+		DeleteBookmark(recipe); // 重複しているブックマークの場合は元ブックマークを削除
 		try {
 			int count = 1;
+			// Bookmark.csvに追記
 			File file = new File("csv/Bookmark.csv");
 			FileWriter filewriter = new FileWriter(file, true);
+			// リスト内レシピをcsv形式に書き込み
 			for (String str : recipe) {
+				// 各データにコンマを加えて書き込み
 				if (count <= recipe.size() - 1)
 					filewriter.write(str + ",");
 				count++;
 			}
-			filewriter.write(recipe.get(recipe.size() - 1) + "\n");
+			filewriter.write(recipe.get(recipe.size() - 1) + "\n"); // 改行を加えて書き込み
 			filewriter.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
+	// ブックマーク削除処理
 	public static void DeleteBookmark(ArrayList<String> recipe) {
+		boolean change = false; // 削除対象があるかどうか
 		try {
-			ArrayList<ArrayList<String>> bookmarkList = new ArrayList<ArrayList<String>>();
+			// Bookmark.csvファイル読み込み
 			FileInputStream fisRec = new FileInputStream("csv/Bookmark.csv");
 			InputStreamReader isrRec = new InputStreamReader(fisRec);
 			BufferedReader brRec = new BufferedReader(isrRec);
+
 			String tempData;
+			ArrayList<ArrayList<String>> bookmarkList = new ArrayList<ArrayList<String>>(); // 削除後レシピリスト代入用
 
+			// リスト代入処理
 			while ((tempData = brRec.readLine()) != null) {
-
 				String[] tempList = tempData.split(",");
 				ArrayList<String> tempArrayList = new ArrayList<String>();
 				for (String tempLi : tempList) {
 					tempArrayList.add(tempLi);
 				}
+				// メニュー名と炭水化物量が合致しない場合に追加
 				if (!(recipe.get(0).equals(tempArrayList.get(0)) && recipe.get(8).equals(tempArrayList.get(8)))) {
 					bookmarkList.add(tempArrayList);
+				} else {
+					change = true;
 				}
 			}
 
 			brRec.close();
 
-			File file = new File("csv/Bookmark.csv");
-			FileWriter filewriter = new FileWriter(file, false);
-			filewriter.write("");
-			filewriter.close();
+			// 削除対象があった場合
+			if (change) {
+				// Bookmark.csv初期化
+				File file = new File("csv/Bookmark.csv");
+				FileWriter filewriter = new FileWriter(file, false);
+				filewriter.write("");
+				filewriter.close();
 
-			for (ArrayList<String> list : bookmarkList) {
-				AddBookmark(list);
+				// 対象外レシピの追加処理実行
+				for (ArrayList<String> list : bookmarkList) {
+					AddBookmark(list);
+				}
 			}
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
