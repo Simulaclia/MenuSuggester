6c39b734c82fc0d382f246aa97b00528acf03c5a
Selelium <65858392+Selelium@users.noreply.github.com>
2020-07-15 11:12:04 +0900

    WeekSuggestPage修正

--- src/top/weeksuggest/WeekSuggestPageContoller.java
+++ src/top/weeksuggest/WeekSuggestPageContoller.java
@@ -13,10 +13,12 @@ import top.bookmark.BookmarkPage;
 public class WeekSuggestPageContoller {
 
 	// ラベルの取得
-	private Label CookMenu1, CookMenu2, CookMenu3, CookMenu4, CookMenu5, CookMenu6, CookMenu7, Genre1, Genre2, Genre3,
-			Genre4, Genre5, Genre6, Genre7, cooking1, cooking2, cooking3, cooking4, cooking5, cooking6, cooking7, time1,
-			time2, time3, time4, time5, time6, time7, calorie1, calorie2, calorie3, calorie4, calorie5, calorie6,
-			calorie7, WACb, YOCb, CHUCb;
+//	private Label CookMenu1, CookMenu2, CookMenu3, CookMenu4, CookMenu5, CookMenu6, CookMenu7, Genre1, Genre2, Genre3,
+//			Genre4, Genre5, Genre6, Genre7, cooking1, cooking2, cooking3, cooking4, cooking5, cooking6, cooking7, time1,
+//			time2, time3, time4, time5, time6, time7, calorie1, calorie2, calorie3, calorie4, calorie5, calorie6,
+//			calorie7, WACb, YOCb, CHUCb;
+	@FXML
+	private Label CookMenu1, CookMenu2, CookMenu3, CookMenu4, CookMenu5, CookMenu6, CookMenu7;
 
 	Random rand = new Random();
 
@@ -79,11 +81,13 @@ public class WeekSuggestPageContoller {
 	@FXML
 	void initialize() {
 		randomRecipe = new ArrayList<ArrayList<String>>();
-		Label[][] label = { { CookMenu1, CookMenu2, CookMenu3, CookMenu4, CookMenu5, CookMenu6, CookMenu7 },
-				{ Genre1, Genre2, Genre3, Genre4, Genre5, Genre6, Genre7 },
-				{ cooking1, cooking2, cooking3, cooking4, cooking5, cooking6, cooking7 },
-				{ time1, time2, time3, time4, time5, time6, time7 },
-				{ calorie1, calorie2, calorie3, calorie4, calorie5, calorie6, calorie7 } };
+//		Label[][] label = { { CookMenu1, CookMenu2, CookMenu3, CookMenu4, CookMenu5, CookMenu6, CookMenu7 },
+//				{ Genre1, Genre2, Genre3, Genre4, Genre5, Genre6, Genre7 },
+//				{ cooking1, cooking2, cooking3, cooking4, cooking5, cooking6, cooking7 },
+//				{ time1, time2, time3, time4, time5, time6, time7 },
+//				{ calorie1, calorie2, calorie3, calorie4, calorie5, calorie6, calorie7 } };
+
+		Label[][] label = { { CookMenu1, CookMenu2, CookMenu3, CookMenu4, CookMenu5, CookMenu6, CookMenu7 } };
 		ArrayList<ArrayList<String>> Weeksuggest = WeekSuggestPage.Weeksuggest();
 		RandomSetMenu(Weeksuggest, label);
 	}
