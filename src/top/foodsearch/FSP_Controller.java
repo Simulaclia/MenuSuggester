53508ce33e7eba96fa691e92831b63ba63747a30
sintomo <50051678+sintomo@users.noreply.github.com>
2020-07-17 10:50:06 +0900

    処理変更

--- src/top/foodsearch/FSP_Controller.java
+++ src/top/foodsearch/FSP_Controller.java
@@ -6,6 +6,9 @@ import top.TopPage;
 
 public class FSP_Controller {
 
+	@FXML //コンボボックスの型
+	private ComboBox<String> FoodList;//選択食材リスト
+
 	static String[] SearchFoodData; // 選択食材データ
 
 	@FXML // Topボタン
@@ -15,27 +18,21 @@ public class FSP_Controller {
 
 	@FXML // 「食材決定ボタン」食材検索処理開始へ行き→ansへ遷移
 	void AnsPage_OnClick() {
-
 		SearchFoodData = new String[] { FoodList.getValue() }; // 選択食材データ代入
 		new TopPage().changePage("/top/foodsearch/FSP_ans.fxml");
 	}
 
-	@FXML //コンボボックスの型
-	private ComboBox<String> FoodList;
-
 	@FXML //選択食材リスト
 	void initialize() {
-
-		// コンボボックスに項目を追加
+		// リストに食材を追加
 		FoodList.getItems().addAll("食材を選択", "豚ひき肉", "キャベツ", "きくらげ", "タケノコ", "オイスターソース", "ねぎ", "きゅうり", "卵", "にんじん",
 				"片栗粉", "醤油", "出汁", "味噌", "麺つゆ", "塩", "鰹節", "人参", "卵白", "里芋", "アスパラ", "ピーマン", "小麦粉", "豚バラ", "鶏もも",
 				"しょうゆ", "豚肉", "マヨネーズ", "たまご", "たまねぎ", "ベーコン", "鳥もも肉", "ひき肉", "グリンピース", "バター", "じゃがいも", "鶏肉", "パン粉",
 				"コーン", "デミグラスソース", "パルメザンチーズ", "ケチャップ", "なす", "酢"); // 選択食材ども
-
 		FoodList.getSelectionModel().select(0); // 初期値
 	}
 
-	// 絞り込み条件データ取得処理
+	// 選択食材データ取得処理
 	static String[] getSearchFoodData() {
 		return SearchFoodData;
 	}
--- src/top/foodsearch/FoodSearchPage.java
+++ src/top/foodsearch/FoodSearchPage.java
@@ -7,25 +7,22 @@ import java.util.ArrayList;
 
 public class FoodSearchPage {
 
-	public static void main(String[] args) {
+	public static ArrayList<ArrayList<String>> FoodSearch(String FoodList) {
 
 		//System.out.println("食材を入力してください。");
-
 		//Scanner Search = new Scanner(System.in);
 		//String MainFood = Search.nextLine();
-
-		String[] SearchFoodData = null;
 
 		try {
 			//読み込みファイルのインスタンス生成
 			FileInputStream fis = new FileInputStream("csv/Recipe.csv");//ファイル名・場所を指定
 			InputStreamReader isr = new InputStreamReader(fis);
 			BufferedReader br = new BufferedReader(isr);
+
 			String tempData;//Recipe.csv読み込み
 			ArrayList<ArrayList<String>> recipeList = new ArrayList<ArrayList<String>>();//Recipe.csv絞り込み後データ
 
 			// レシピCSVファイル絞り込み処理
-			String[] arr = null;
 			while ((tempData = br.readLine()) != null) {// CSVファイル内データの最後までループ
 				String[] tempList = tempData.split(",");// CSVファイル一行分から","で分割して配列代入
 				ArrayList<String> tempArrayList = new ArrayList<String>();// 1行代入用
@@ -33,7 +30,7 @@ public class FoodSearchPage {
 					tempArrayList.add(tempLi);
 				}
 				//メイン食材絞り込み
-				if (tempArrayList.get(9).equals(SearchFoodData)) {// 合致する食材を検索
+				if (tempArrayList.get(9).equals(FoodList)) {// 合致する食材を検索
 					recipeList.add(tempArrayList);// 合致したレシピを追加
 				}
 			}
@@ -45,8 +42,10 @@ public class FoodSearchPage {
 				System.out.println();
 			}
 			br.close();
+			return null;
 		} catch (Exception e) {
 			e.printStackTrace();
+			return null;
 		}
 	}
 }
