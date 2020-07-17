5b403c883d4aac557fe0b8d02d600b7153dff534
toraneko946 <66046897+toraneko946@users.noreply.github.com>
2020-07-17 23:10:29 +0900

    1週間ランダム表示 進捗 処理あと少し

--- src/top/weeksuggest/WeekSuggestPage.java
+++ src/top/weeksuggest/WeekSuggestPage.java
@@ -4,9 +4,11 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class WeekSuggestPage {
 
+	//通常の処理
 	public static ArrayList<ArrayList<String>> Weeksuggest() {
 		try {
 			// レシピCSVファイル読み込み処理
@@ -30,6 +32,99 @@ public class WeekSuggestPage {
 
 			brRec.close();
 			return recipeList;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	//絞り込み条件有りの場合
+	public static ArrayList<ArrayList<String>> Weeksuggest(String genre1, String genre2, String genre3,
+			String price) {
+		//ジャンル(国)分けするための二次元配列の宣言
+		ArrayList<ArrayList<String>> Japanese_foodArray = new ArrayList<ArrayList<String>>();
+		ArrayList<ArrayList<String>> Western_foodArray = new ArrayList<ArrayList<String>>();
+		ArrayList<ArrayList<String>> ChineseArray = new ArrayList<ArrayList<String>>();
+		//ジャンル(国)分けした後の絞り込み用配列の宣言
+		//ArrayList<String> NarrowDownData = new ArrayList<String>();
+
+		try {
+			FileInputStream fisRec = new FileInputStream("csv/Recipe.csv");
+			InputStreamReader isrRec = new InputStreamReader(fisRec);
+			BufferedReader brRec = new BufferedReader(isrRec);
+
+			String tempData;
+			ArrayList<ArrayList<String>> NarrowDownList = new ArrayList<ArrayList<String>>();
+
+			Random rand = new Random();
+
+			while ((tempData = brRec.readLine()) != null) {
+				String[] tempList = tempData.split(",");
+				ArrayList<String> tempArrayList = new ArrayList<String>();
+				for (String tempLi : tempList) {
+					tempArrayList.add(tempLi);
+				}
+				// 絞り込み処理
+				/*if ((tempArrayList.get(1).equals(genre1) || genre1.equals("選択なし")) // ジャンル絞り込み
+						&& (tempArrayList.get(1).equals(genre2) || genre2.equals("選択なし")) // 料理国絞り込み
+						&& (genre3.equals("選択なし") || (Integer.parseInt(tempArrayList.get(5)) <= (Integer.parseInt(genre3)))) // カロリー絞り込み
+						&& (price.equals("選択なし")
+								|| (Integer.parseInt(tempArrayList.get(3)) <= (Integer.parseInt(price)))) // 価格絞り込み
+				) */
+
+				//ジャンル(国)分け
+				//for() {
+
+				switch (tempArrayList.get(1)) {
+				case "和食":
+
+					Japanese_foodArray.add(tempArrayList);
+				case "洋食":
+
+					Western_foodArray.add(tempArrayList);
+				case "中華":
+
+					ChineseArray.add(tempArrayList);
+				}
+			}
+
+			for (int i = 1; i <= Integer.parseInt(genre1); i++) {
+				int randomValue = rand.nextInt(Japanese_foodArray.size());
+				NarrowDownList.add(Japanese_foodArray.get(randomValue));
+			}
+			for (int i = 1; i <= Integer.parseInt(genre2); i++) {
+				int randomValue = rand.nextInt(Western_foodArray.size());
+				NarrowDownList.add(Western_foodArray.get(randomValue));
+			}
+			for (int i = 1; i <= Integer.parseInt(genre3); i++) {
+				int randomValue = rand.nextInt(ChineseArray.size());
+				NarrowDownList.add(ChineseArray.get(randomValue));
+			}
+
+			//}
+
+			/*
+				switch (tempArrayList.get(1)) {
+				case "和食":
+					for(int i = 1 ; i <= genre1; i++) {
+			
+					}
+					;
+				case "洋食":
+					for(int i = 1 ; i <= genre2; i++) {
+			
+					}
+			
+					;
+				case "中華":
+					for(int i = 1 ; i <= genre3; i++) {
+			
+					}
+			
+					;
+				}*/
+			brRec.close();
+			return NarrowDownList;
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
