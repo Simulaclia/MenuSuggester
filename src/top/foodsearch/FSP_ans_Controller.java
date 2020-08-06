25e31dd0a64ecfb948848b70103c35b95e3bcc83
sintomo <50051678+sintomo@users.noreply.github.com>
2020-08-06 12:02:28 +0900

    shuusei

--- src/top/foodsearch/FSP_ans_Controller.java
+++ src/top/foodsearch/FSP_ans_Controller.java
@@ -13,7 +13,7 @@ public class FSP_ans_Controller {
 	@FXML
 	private Label CookMenu1, Genre1, cooking1, calorie1, time1;
 
-	ArrayList<ArrayList<String>> FSPRecipe;
+	ArrayList<ArrayList<String>> fspRecipe;
 	ArrayList<ArrayList<String>> FSPMenu;
 	Random rand = new Random();
 
@@ -29,7 +29,7 @@ public class FSP_ans_Controller {
 
 	@FXML //選択食材のレシピを貼り付け
 	void initialize() {
-		FSPRecipe = new ArrayList<ArrayList<String>>();
+		fspRecipe = new ArrayList<ArrayList<String>>();
 		Label[][] label = { { CookMenu1 }, { cooking1 }, { Genre1 }, { time1 }, { calorie1 } };
 		FSPMenu = FoodSearchPage.FoodSearch();//レシピを持ってくる
 		//System.out.println(FSPMenu + "\t");
@@ -38,7 +38,7 @@ public class FSP_ans_Controller {
 
 	@FXML
 	void addBookmark_OnClick() {
-		BookmarkPage.AddBookmark(FSPRecipe.get(0));
+		BookmarkPage.AddBookmark(fspRecipe.get(0));
 	}
 
 	void FSPSetMenu(ArrayList<ArrayList<String>> recipeList, Label[][] label) {
@@ -50,5 +50,7 @@ public class FSP_ans_Controller {
 		label[2][0].setText(recipeList.get(randomValue).get(2)); // 料理ジャンル
 		label[4][0].setText(recipeList.get(randomValue).get(3)); // 所要時間
 		label[3][0].setText(recipeList.get(randomValue).get(4)); // カロリー
+
+		fspRecipe.add(recipeList.get(randomValue));
 	}
 }
