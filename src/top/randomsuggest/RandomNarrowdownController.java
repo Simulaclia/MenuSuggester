4fcfa2bb21ca8057b2d5ba244a38dde2c176977f
The-Reidon <66047096+The-Reidon@users.noreply.github.com>
2020-08-06 09:54:53 +0900

    絞り込み&ランダム献立提案 骨組みは作成出来たかも

--- src/top/randomsuggest/RandomNarrowdownController.java
+++ src/top/randomsuggest/RandomNarrowdownController.java
@@ -1,31 +1,23 @@
 package top.randomsuggest;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import top.TopPage;
 
 public class RandomNarrowdownController {
 
-	//メモ
-	/*
-	 * ラベルの中身を取得した後、getなにがしで返す
-	 * 決定ボタンに「取得+その値を返す処理+ページ遷移」する処理を追加(Narrowdownの方で)
+	/*メモ
+	 * ラベルの中身を取得した後、getで返す
+	 * 決定ボタンに「取得+その値を返す処理+ページ遷移」する処理を追加
 	 * 受け取った値を処理するコードをrandomsuggestのcontrollerとかで記述
 	 */
 
-	//データ格納
+	//絞り込みデータ格納
 	public static String[] StrNarrowDate;
-	public static int[] intNarrowDate;
-
-	@FXML //ページ遷移
-	void ReturnRandomSuggestPage_OnClick() {
-		new TopPage().changePage("/top/randomsuggest/RandomSuggestPage.fxml");
-	}
 
 	@FXML //comboboxの取得
-	public ComboBox<String> genrecombo, cookingcombo;
-
-	public ComboBox<Integer> price, kcal, time;
+	public ComboBox<String> genrecombo, cookingcombo, price, kcal, time;
 
 	@FXML //初期値設定(combobox)
 	public void initialize() {
@@ -36,13 +28,13 @@ public class RandomNarrowdownController {
 		cookingcombo.getItems().addAll("揚げ物", "煮物", "焼き物", "炒め物", "汁物");
 
 		//価格
-		price.getItems().addAll(700, 600, 500);
+		price.getItems().addAll("700", "600", "500");
 
 		//カロリー
-		kcal.getItems().addAll(500, 400, 300);
+		kcal.getItems().addAll("500", "400", "300");
 
 		//所要時間
-		time.getItems().addAll(60, 50, 40);
+		time.getItems().addAll("60", "50", "40");
 	}
 
 	@FXML //リセットの処理
@@ -54,21 +46,24 @@ public class RandomNarrowdownController {
 		time.getSelectionModel().select(0);
 	}
 
-	@FXML //決定
-	void okbuttonNarrowdown() {
-		StrNarrowDate = new String[] { genrecombo.getValue(), cookingcombo.getValue() };
-		intNarrowDate = new int[] { price.getValue(), kcal.getValue(), time.getValue() };
+	@FXML //決定の処理 取得+その値を返す処理+ページ遷移
+	//ダメだったら()内の処理を消しておく
+	void okbuttonNarrowdown_OnClick(ActionEvent evt) {
+		StrNarrowDate = new String[] { genrecombo.getValue(), cookingcombo.getValue(),
+				price.getValue(), kcal.getValue(), time.getValue() };
+
+		new TopPage().changePage("/top/randomsuggest/RandomSuggestPage.fxml");
+	}
+
+	@FXML //ページ遷移
+	void ReturnRandomSuggestPage_OnClick() {
+		new TopPage().changePage("/top/randomsuggest/RandomSuggestPage.fxml");
 	}
 
 	@FXML //データ取得
 	static String[] getStrNarrowDate() {
 		return StrNarrowDate;
 
-	}
-
-	@FXML //データ(数値)
-	static int[] getintNarrowDate() {
-		return intNarrowDate;
 	}
 
 }
